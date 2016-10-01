98174047d4605cdb60ecccc815a51e3514651579
Ramit Suri <ramitsuri007@gmail.com>
2016-09-30 21:18:45 -0400

    changed port

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var activityRoutes = require('./routes/activity.js');
 var buttonActionRoutes = require('./routes/buttonActions.js');
 var app = express();
-var port = 1398;
+var port = 80;
 app.use('/activity', activityRoutes);
 app.use('/actions', buttonActionRoutes);
 
